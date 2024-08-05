e01a59a16144dcf9cfa87ecddb0f3b7d3019005f
menega2010 <menega2010@gmail.com>
2024-08-05 13:14:09 -0300

    Config front da aplicação e API - 01
    Criado teamplate para o front da aplicação, e gerado a base da API da aplicação.

---
+++ front-portal-alunos/src/components/TableAlunos.vue
@@ -0,0 +1,20 @@
+<v-data-table :items="items"></v-data-table>
+
+
+
+<script setup>
+  const items = [
+    {
+      name: 'African Elephant',
+      species: 'Loxodonta africana',
+      diet: 'Herbivore',
+      habitat: 'Savanna, Forests',
+    },
+  ]
+</script>
+
+<script>
+export default {
+  name: 'TableAlunos'
+}
+</script>
